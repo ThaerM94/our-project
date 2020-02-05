b921eac8105c47ba61a9ca7e7f97b82964ee67ec
thaer <thaer1994.tm98@yahoo.com>
2020-02-05 13:00:03 +0200

    done

--- add-js/new.js
+++ add-js/new.js
@@ -1,10 +1,17 @@
+function mark () {
+
 var exam = prompt ("what is your mark ?")
-console.log
+console.log(exam);
 var interview = prompt (" did you pass the interview ?");
+console.log(interview);
 var age = prompt (" what is your age ");
+console.log(age);
 var result;
 if (exam >= 25 && interview >= 80 && age >= 24  ) {
     result = 'passed'
  } else {result = 'fail'
+ 
  }
-document.write('<h3>'+result+'</h3>');
+ return result;
+}
+document.write('<h3>'+mark()+'</h3>');
